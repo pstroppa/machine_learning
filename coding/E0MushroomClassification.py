8315866ba91e3f152b9bba5d18887fa52df6921e
Peter Stroppa <peter@stroppa.at>
2019-10-15 00:31:39 +0200

    adapted pics

--- coding/E0MushroomClassification.py
+++ coding/E0MushroomClassification.py
@@ -38,26 +38,29 @@ for elem in samples:
 
 source = ColumnDataSource(data=dict(classes=classes, counts=values, color=Spectral6[0:2]))
 p = figure(x_range=classes, plot_height=500, title="Distribution Mushrooms", x_axis_label=('Edibility'),y_axis_label=('Number of samples'), toolbar_location=None)
-p.vbar(x='classes', top='counts', width=0.9, color='color', legend="classes", source=source)
+p.vbar(x='classes', top='counts', width=0.9, color='color', source=source) #legend ="classes"
 
 p.xgrid.grid_line_color = "white"
 p.y_range.start = 0
 
 p.title_location = "above"
 p.title.align = 'center'
-p.title.text_font_size = '16pt'
+p.title.text_font_size = '20pt'
 
-p.xaxis.axis_label_text_font_size = "16pt"
-p.yaxis.axis_label_text_font_size = "16pt" 
-p.legend.label_text_font_size = "16pt"
+p.xaxis.axis_label_text_font_size = "20pt"
+p.yaxis.axis_label_text_font_size = "20pt" 
+p.xaxis.major_label_text_font_size = "16pt"
+
+
+#p.legend.label_text_font_size = "16pt"
 p.y_range=Range1d(0,5000)
 
-p.legend.location = "top_left"
-p.legend.orientation = "horizontal"
-p.legend.location = "top_center"
+#p.legend.location = "top_left"
+#p.legend.orientation = "horizontal"
+#p.legend.location = "top_center"
 
-citation1 = Label(x=110, y=350, x_units='screen', y_units='screen', text=str(values[0]), text_color = "white")
-citation2 = Label(x=360, y=320, x_units='screen', y_units='screen', text=str(values[1]), text_color = "white")
+citation1 = Label(x=100, y=285, x_units='screen', y_units='screen', text=str(values[0]), text_color = "white", text_font_size = "20pt")
+citation2 = Label(x=360, y=265, x_units='screen', y_units='screen', text=str(values[1]), text_color = "white", text_font_size = "20pt")
 
 p.add_layout(citation1)
 p.add_layout(citation2)
--- coding/E0WineClassification.py
+++ coding/E0WineClassification.py
@@ -23,7 +23,6 @@ from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, Label, Range1d
 from bokeh.palettes import Spectral11
 
-"""Wine Quality"""
 
 filename='data/wine_red.csv'
 
@@ -37,7 +36,7 @@ for elem in samples:
 
 source = ColumnDataSource(data=dict(classes=classes, counts=values, color=Spectral11))
 p = figure(x_range=classes, plot_height=500, title="Distribution Wine Quality", x_axis_label=('Quality Value'),y_axis_label=('Number of samples'), toolbar_location=None)
-p.vbar(x='classes', top='counts', width=0.9, color='color', legend="classes", source=source)
+p.vbar(x='classes', top='counts', width=0.9, color='color', source=source) #legend="classes"
 
 p.xgrid.grid_line_color = "white"
 p.y_range.start = 0
@@ -48,21 +47,29 @@ p.title.text_font_size = '16pt'
 
 p.xaxis.axis_label_text_font_size = "16pt"
 p.yaxis.axis_label_text_font_size = "16pt" 
-p.legend.label_text_font_size = "16pt"
+p.xaxis.major_label_text_font_size = "14pt"
+
+#p.legend.label_text_font_size = "16pt"
 p.y_range=Range1d(0,850)
 
-p.legend.location = "top_left"
-p.legend.orientation = "horizontal"
-p.legend.location = "top_center"
+#p.legend.location = "top_left"
+#p.legend.orientation = "horizontal"
+#p.legend.location = "top_center"
 
 for i in range(11):
-    if i == 0:
-        p.add_layout(Label(x=20, y=int(values[i]), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
-    elif values[i]<10:
-        p.add_layout(Label(x=i*52, y=int(values[i]), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
+    if i < 3:
+        p.add_layout(Label(x=20+49*i, y=int(values[i]), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))   
+    elif i == 9 or i ==10:
+        p.add_layout(Label(x=17+49*i, y=int(values[i]), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))   
+    elif i==4:
+        p.add_layout(Label(x=15+49*i, y=int(values[i]/2), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
+    elif values[i]<20:
+        p.add_layout(Label(x=13+49*i, y=int(values[i]-8), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
     elif values[i]<100:
-        p.add_layout(Label(x=i*51, y=int(values[i]/2), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
+        p.add_layout(Label(x=10+49*i, y=int(values[i]/2), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
+    elif values[i]<200:
+        p.add_layout(Label(x=9+49*i, y=int(values[i]/2.09), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
     else:
-        p.add_layout(Label(x=i*50, y=int(values[i]/2.09), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
+        p.add_layout(Label(x=10+49*i, y=int(values[i]/2.09), x_units='screen', y_units='screen', text=str(values[i]), text_color = "black"))
 
 export_png(p, filename="pics/distribution_winequality.png")
