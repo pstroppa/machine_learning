446a458d536c2390ce5908ddf383dbe0ef283c27
Peter Stroppa <peter@stroppa.at>
2019-12-08 19:58:41 +0100

    wine parameters finished

--- E2/coding/E2_wine.py
+++ E2/coding/E2_wine.py
@@ -34,7 +34,7 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.datasets import make_classification
 from sklearn.svm import LinearSVC
 from sklearn import svm
-
+from sklearn.utils import shuffle
 #import E2.coding.find_distribution_short as fds
 ######################################################################
 #comments
@@ -45,12 +45,12 @@ from sklearn import svm
 #Input
 
 filename = "wine_red.csv"
-methode = "knn"
-cross_validation = 7
+methode = "svm"
+cross_validation = 6
 data_df = pd.read_csv("E2/data/" + filename, sep=";",
                       lineterminator="\n", encoding="utf-8", error_bad_lines=False)
-
-data_df= data_df.sample(frac=1)
+data_df = shuffle(data_df)
+#data_df= data_df.sample(frac=1)
 ######################################################################
 
 # find distribution that best suits distribution of attributes:
@@ -91,11 +91,6 @@ X3 = minmax.transform(X1)  # X3 minmax scaling
 
 X4 = X1.copy()
 
-#names=top5_corr.index.tolist()
-#for col in names:
-#    X4[col] = X4[col] / max(X4[col])*abs(corr_feature[col])
-
-
 for col in X4.columns:
     if corr_feature[col] < 0:
         X4[col] = (max(X4[col])-X4[col])/max(X4[col])*abs(corr_feature[col])
@@ -118,13 +113,19 @@ for col in X4.columns:
 #select methode
 if type(methode) is str:
     if methode is "forest":
-        methode = RandomForestClassifier(n_estimators=100)#, random_state=0)
+        methode = RandomForestClassifier(n_estimators=100, max_features=6)
+        # like this best! prep 3 / 4 all 1
+        #max_depth=10, min_samples_split=2, min_samples_leaf=4 did not change anything
+        # min_samples_leaf= 100 seems to be best.
     elif methode is "knn":
-        k = 5
+        k = 1
         weigh = "uniform"
         methode = neighbors.KNeighborsClassifier(k, weights=weigh)
+        # 2 splits not full 1 best prep 4
     elif methode is"svm":
-        methode = svm.SVC(gamma='scale')
+        methode = svm.SVC(gamma="scale", kernel="poly", degree=4)
+        # poly best with prep 4 and degree = 4 all 1
+        # best prep =4
     else:
         print("Error: Wrong methode chosen!")
 
@@ -134,30 +135,34 @@ score = {'accuracy': make_scorer(accuracy_score),
          'precision': make_scorer(precision_score, average='macro'),
          'recall': make_scorer(recall_score, average='macro')}
  
-np.set_printoptions(precision = 7)
+np.set_printoptions(precision = 3)
 
 #Fit of 1
-
 cv_results1 = cross_validate(methode, X1, y, scoring=score, cv=cross_validation)
 for key1 in list(cv_results1.keys())[2:]:
-    print('{:35}'.format('evalution for 1, ' + key1 + ": "), cv_results1[key1])
+    cv_results1[key1] = np.append(cv_results1[key1], cv_results1[key1].mean())
+    print('{:35}'.format('evaluation for 1, ' + key1 + ": "), cv_results1[key1])
 print("\n")
 #Fit of 2
 cv_results2 = cross_validate(methode, X2, y, scoring = score,  cv=cross_validation)
 for key2 in list(cv_results2.keys())[2:]:
-    print('{:35}'.format('evalution for 2, ' + key2 + ": "), cv_results2[key2])
+    cv_results2[key2] = np.append(cv_results2[key2], cv_results2[key2].mean())
+    print('{:35}'.format('evaluation for 2, ' + key2 + ": "), cv_results2[key2])
 print("\n")
 
 #Fit of 3
 cv_results3 = cross_validate(methode, X3, y, scoring = score,  cv=cross_validation)
 for key3 in list(cv_results3.keys())[2:]: 
-    print('{:35}'.format('evalution for 3, ' + key3 + ": "), cv_results3[key3])
+    cv_results3[key3] = np.append(cv_results3[key3], cv_results3[key3].mean())
+    print('{:35}'.format('evaluation for 3, ' + key3 + ": "), cv_results3[key3])
 
 print("\n")
 #Fit of 4 ..
 cv_results4 = cross_validate(methode, X4, y, scoring=score,  cv=cross_validation)
 for key4 in list(cv_results4.keys())[2:]:
-    print('{:35}'.format('evalution for 4, ' + key4 + ": "), cv_results4[key4])
+    cv_results4[key4] = np.append(cv_results4[key4], cv_results4[key4].mean())
+    print('{:35}'.format('evaluation for 4, ' + key4 + ": "), cv_results4[key4])
+print("\n")
 
 
 
