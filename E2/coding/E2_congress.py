6f1e570a7d9f177d493d007df3d0224a00e71b7a
LuniTU <luni@live.at>
2019-12-08 21:19:15 +0100

    1

--- E2/coding/E2_congress.py
+++ E2/coding/E2_congress.py
@@ -18,29 +18,14 @@
     8) evaluation
 """
 import numpy as np
-from math import sqrt
 import pandas as pd
-#import machine learning packages
-from sklearn import linear_model, neighbors
-from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
-from sklearn import preprocessing, tree
-from sklearn.compose import ColumnTransformer
-from sklearn.model_selection import train_test_split
-
-
-#from sklearn import datasets, linear_model
+from sklearn.metrics.scorer import make_scorer,accuracy_score,precision_score,recall_score
 from sklearn.model_selection import cross_validate
-#from sklearn.metrics.scorer import make_scorer
-from sklearn.metrics import confusion_matrix
-#from sklearn.svm import LinearSVC
 from sklearn import svm
-
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.datasets import make_classification
-
+from sklearn.utils import shuffle
+from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
-
-
 
 ######################################################################
 #comments
@@ -51,12 +36,35 @@ from sklearn.neighbors import KNeighborsClassifier
 #Input
 
 filename = 'congress_train.csv'
-methode = "knn"
+methode = "svm"
 
 cross_validation = 5
 
 data_df = pd.read_csv("Data1/"+filename, sep=",",
                         lineterminator="\n", encoding="utf-8", error_bad_lines=False)
+
+
+
+
+A = data_df.copy()
+
+A=A.drop(columns= ['ID'])
+A=A.replace(to_replace='democrat',value=1)
+A=A.replace(to_replace='republican',value=0)
+
+
+A=A.replace(to_replace='y',value=1)
+A=A.replace(to_replace='n',value=0)
+A=A.replace(to_replace='unknown',value=0.5)
+
+#data_df=data_df.replace(to_replace='unknown',value=0.5)
+
+corr = A.corr()
+corr_feature = corr["class"].sort_values(ascending=False)
+top5_corr = abs(A.corr()["class"]).sort_values(ascending=False)[1:6]
+
+
+#data_df = shuffle(data_df)
 
 ######################################################################
 #general preprocessing (not associated with any methode or any of the four preprocessing methods later on)
@@ -64,51 +72,43 @@ data_df = pd.read_csv("Data1/"+filename, sep=",",
 #ID dropped, since it contains no relevant information
 data_df = data_df.drop(columns=['ID'])
 
+X1 = data_df.copy()
+X1 = X1.drop(columns=['class'])
+
 #ordinal encoded
 data_df=data_df.replace(to_replace='y',value=1)
 data_df=data_df.replace(to_replace='n',value=0)
 data_df=data_df.replace(to_replace='unknown',value=0.5)
-data_df = data_df.replace(to_replace='democrat',value = 1)
-data_df = data_df.replace(to_replace='republican',value = 0)
+
+#data_df = data_df.replace(to_replace='democrat',value = 1)
+#data_df = data_df.replace(to_replace='republican',value = 0)
 
 
 
 
-# get target value
+#get target value
 y = data_df['class']
-# drop unimportant columns
+#drop unimportant columns
 X = data_df.drop(columns=['class'])
 
 ######################################################################
-#calculate Correlation
-corr = data_df.corr()
-corr_feature = corr['class'].sort_values(ascending=False)
-#print(corr_feature)
-######################################################################
-#prediction calculation (20 times)
 
-#counter which Preprocessing Type wins the most and which loses the most
-
-
-
-######################################################################
 #preprocessing
 #x1  , x2 , x3 , x4 
 
-X1 = data_df.copy()
+enc = preprocessing.OneHotEncoder(handle_unknown='ignore')
+enc.fit(X1)
+#D3 = enc.transform(X3).toarray()
+
+X1 = enc.transform(X1)
 
 
-
-
-X2 = data_df.copy()
-
-
-
+X2 = X.copy()
 
 
 #feature selection: die mit niedrigster Korrelation werden eliminiert: hier bei <6% sind das
 #erstaunlicher Weise die Spalten immigration und water-project-sharing-cost
-X3 = data_df.copy()
+X3 = X.copy()
 #X3 = X3[["adoption-of-the-budget-resolution",'physician-fee-freeze']]   #.drop(columns=['immigration','water-project-cost-sharing'])
 
 
@@ -119,22 +119,21 @@ X3 = X3.drop(columns=['immigration','water-project-cost-sharing'])
 
 
 
-X4 = data_df.copy()
+X4 = X.copy()
+
+for col in X4.columns:
+    if corr_feature[col] < 0:
+        X4[col] = (max(X4[col])-X4[col])/max(X4[col])*abs(corr_feature[col])
+    else:
+        X4[col] = X4[col] / max(X4[col])*abs(corr_feature[col])
+
 
 
 #Anmerkung: An Korrelationsmatrix sieht man, dass Klassen mit gewissen Attributen zu 90% korrellieren können
 #Vermutung: Decision-Trees gut für so eine Aufgabe
 #
-C= data_df.corr()
-C = C['class'].sort_values()
-
-
-
-
-
-
-
-
+#C= data_df.corr()
+#C = C['class'].sort_values()
 
 
 
@@ -146,20 +145,20 @@ C = C['class'].sort_values()
 
     #select methode
 if type(methode) is str:
-    if methode is "tree":
+    if methode is "forest":
         #Tx, Ty = make_classification(n_samples=1000, n_features=4,
         #                             n_informative=2, n_redundant=0,
         #                             random_state=0, shuffle=False))
         
         
-        methode = RandomForestClassifier(max_depth=2, random_state=0)
+        methode = RandomForestClassifier(n_estimators=100)
         #methode.fit(Tx,Ty)
     elif methode is "knn":
-        k = 5
-        weigh = "uniform"
-        methode = neighbors.KNeighborsClassifier(k, weights=weigh)
+        k = 1
+        weigh = "distance"
+        methode = KNeighborsClassifier(k, weights=weigh)
     elif methode is"svm":
-        methode = svm.SVC(gamma='scale')
+        methode = svm.SVC(kernel='poly',coef0 = 10,cache_size=400,gamma='auto')
     else:
         print("Error: Wrong methode chosen!")
 
@@ -171,40 +170,42 @@ if type(methode) is str:
 
 
 
-score = ['accuracy', 'precision', 'recall']
-np.set_printoptions(precision = 7)
+#score = ['accuracy', 'precision_weighted', 'recall_weighted']
+
+score = {'accuracy': make_scorer(accuracy_score),
+         'precision': make_scorer(precision_score, average='macro'),
+         'recall': make_scorer(recall_score, average='macro')}
 
 
 
 
-#Fit of 1
-cv_results = cross_validate(methode, X1, y, scoring = score,  cv=cross_validation)
 
-print('test accuracy for 1: ', cv_results['test_accuracy'])  
-print('test precision for 1: ', cv_results['test_precision']) 
-print('test recall for 1: ', cv_results['test_recall'])
+np.set_printoptions(precision = 4)
 
-
-
+cv_results1 = cross_validate(methode, X1, y, scoring=score, cv=cross_validation)
+for key1 in list(cv_results1.keys())[2:]:
+    cv_results1[key1] = np.append(cv_results1[key1], cv_results1[key1].mean())
+    print('{:35}'.format('evaluation for 1, ' + key1 + ": "), cv_results1[key1])
+print("\n")
 #Fit of 2
-cv_results = cross_validate(methode, X2, y, scoring = score,  cv=cross_validation)
-
-print('test accuracy for 2: ', cv_results['test_accuracy'])  
-print('test precision for 2: ', cv_results['test_precision']) 
-print('test recall for 2: ', cv_results['test_recall'])
-
-
+cv_results2 = cross_validate(methode, X2, y, scoring = score,  cv=cross_validation)
+for key2 in list(cv_results2.keys())[2:]:
+    cv_results2[key2] = np.append(cv_results2[key2], cv_results2[key2].mean())
+    print('{:35}'.format('evaluation for 2, ' + key2 + ": "), cv_results2[key2])
+print("\n")
 
 #Fit of 3
 cv_results3 = cross_validate(methode, X3, y, scoring = score,  cv=cross_validation)
-                         
-print('test accuracy for 3: ', cv_results3['test_accuracy'])  
-print('test precision for 3: ', cv_results3['test_precision']) 
-print('test recall for 3: ', cv_results3['test_recall'])
+for key3 in list(cv_results3.keys())[2:]: 
+    cv_results3[key3] = np.append(cv_results3[key3], cv_results3[key3].mean())
+    print('{:35}'.format('evaluation for 3, ' + key3 + ": "), cv_results3[key3])
 
+print("\n")
 
-#Fit of 4 ..
-
+cv_results4 = cross_validate(methode, X4, y, scoring = score,  cv=cross_validation)
+for key4 in list(cv_results4.keys())[2:]: 
+    cv_results4[key4] = np.append(cv_results4[key4], cv_results4[key4].mean())
+    print('{:35}'.format('evaluation for 4, ' + key4 + ": "), cv_results4[key4])
 
 
 
